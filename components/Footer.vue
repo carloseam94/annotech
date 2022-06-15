b24a3251674ae90acfdd9e7b124425e2569c18cf
Carlos <carloseam94@gmail.com>
2022-06-15 00:34:59 -0400

    style in view source code

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <b-navbar type="dark" variant="dark">
       <b-navbar-nav>
-        <b-nav-item href="https://github.com/carloseam94/annotech" target="_blank"><b-icon icon="github"></b-icon>View source code</b-nav-item>
+        <b-nav-item href="https://github.com/carloseam94/annotech" target="_blank"><b-icon icon="github"></b-icon> View source code</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </footer>
