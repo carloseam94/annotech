b44095540d5168be83969d63ff03ea306cae0bbb
Carlos <carloseam94@gmail.com>
2022-06-14 23:31:34 -0400

    social media meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,73 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      // Twitter
+      // Test on: https://cards-dev.twitter.com/validator
+      {
+        hid: "twitter:card",
+        name: "twitter:card",
+        content: "summary_large_image"
+      },
+      {
+        hid: "twitter:url",
+        name: "twitter:url",
+        content: "https://annotech.netlify.app/"
+      },
+      {
+        hid: "twitter:title",
+        name: "twitter:title",
+        content: "AnnoTech"
+      },
+      {
+        hid: "twitter:description",
+        name: "twitter:description",
+        content: "Annotations on texts and documents."
+      },
+      {
+        hid: "twitter:image",
+        name: "twitter:image",
+        content: "https://annotech.netlify.app/social-media.png"
+      },
+
+      // Open Graph
+      // Test on: https://developers.facebook.com/tools/debug/
+      {
+        hid: "og:site_name",
+        property: "og:site_name",
+        content: "AnnoTech"
+      },
+      { hid: "og:type", property: "og:type", content: "website" },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://annotech.netlify.app/"
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "AnnoTech"
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: "Annotations on texts and documents."
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "https://annotech.netlify.app/social-media.png"
+      },
+      {
+        hid: "og:image:secure_url",
+        property: "og:image:secure_url",
+        content: "https://annotech.netlify.app/social-media.png"
+      },
+      {
+        hid: "og:image:alt",
+        property: "og:image:alt",
+        content: "AnnoTech"
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
