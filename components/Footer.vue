1947c3adadaea7ef4a5895713ab482287a0b658f
Carlos <carloseam94@gmail.com>
2022-06-14 22:37:34 -0400

    github link to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <b-navbar type="dark" variant="dark">
       <b-navbar-nav>
-        <b-nav-item href="#"><b-icon icon="github"></b-icon></b-nav-item>
+        <b-nav-item href="https://github.com/carloseam94/annotech" target="_blank"><b-icon icon="github"></b-icon></b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </footer>
